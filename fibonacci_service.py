d210c6c4dcac1ab4ff3b4d11273ccf3bba8dfb1a
Maggie <maggie.tech.nj@gmail.com>
2021-06-20 00:58:05 +0530

    Add files via upload

---
+++ fibonacci_service.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+#  python -V
+#	Python 2.7.5
+
+
+def valid_input(i):
+	try:
+		float(i) # for int, long and float
+	except ValueError:
+		try:
+			complex(i) # for complex
+		except ValueError:
+			return False
+	if i == int(i):
+		return True
+	return False
+
+
+def fib():
+	x, y = 0, 1
+	while 1:
+		yield x
+		x, y = y, x + y
+
+
+def fibonacci(fib_length):
+	if not valid_input(fib_length):
+		raise ValueError("INVALID Entry:  not an integer")
+
+	if fib_length < 1:
+		raise ValueError("INVALID Input - Enter a value > 0 ") 	
+
+	series = []
+	x = fib()
+	for i in range(fib_length):
+		series.append(next(x))
+	return series
+
+    
---
+++ main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import web
+from fibonacci_service import fibonacci
+
+
+urls = (
+    "/", "index",
+    "/fibonacci/(.*)", "fibonacc"
+)
+
+class index:
+	def GET(self):
+		return 'Fibonacci web service. Append the url with /fibonacci/<number> to generate the series.'
+
+# Input a number (num)
+# Verify (num) is an integer greater than 0 
+# 	If error, return exception message
+# 	If valid entry, call fibonacci(num)
+#	NOTE: there is no upper limit on (num)- could cause memory issues if too large
+class fibonacc:
+	def GET(self, num):
+		try:
+			num = int(num)
+			series = fibonacci(num)
+		except ValueError as e:
+			return """{\n"Exception":"%s"\n}""" % (e) 
+
+		return "%s" % (series)
+
+def main():
+	serv = web.application(urls, globals())
+	serv.run()
+
+if __name__ == '__main__':
+    main()
+    
